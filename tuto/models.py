2de9d7cb03ed5c0fda2214ee69b3726c78c87981
Toniomp <delahayetonio77@gmail.com>
2024-10-15 16:34:32 +0200

    Ajout de la table Genre et de la relation Many-to-Many
    Co-authored-by: Vardanyan Sargis <varuniv@users.noreply.github.com>

--- tuto/models.py
+++ tuto/models.py
@@ -6,6 +6,12 @@ from wtforms import StringField , HiddenField
 from wtforms.validators import DataRequired
 from .app import login_manager
 
+
+book_genre = db.Table('book_genre',
+    db.Column('book_id', db.Integer, db.ForeignKey('book.id')),
+    db.Column('genre_id', db.Integer, db.ForeignKey('genre.id')))
+
+
 class Author (db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column (db.String(100))
@@ -29,10 +35,15 @@ class Book(db.Model):
     author_id = db.Column(db.Integer, db.ForeignKey("author.id"))
     author = db.relationship("Author",
         backref = db.backref("books", lazy="dynamic"))
+    genres = db.relationship('Genre', secondary=book_genre, lazy='subquery',
+                             backref=db.backref('books', lazy=True))
     
     def __repr__ (self ):
             return "<Book (%d) %s>" % (self.id , self.title)
 
+class Genre(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
 
 def get_sample():
     return Book.query.limit(10).all()
