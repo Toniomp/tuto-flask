b4b10445ab00480a427df2c321c9975ac852355b
Toniomp <delahayetonio77@gmail.com>
2024-10-17 16:04:48 +0200

    Test

---
+++ lancement.sh
@@ -0,0 +1,4 @@
+source venv/bin/activate
+cd tuto-flask
+cd tuto 
+flask run 
---
+++ venv.sh
@@ -0,0 +1,9 @@
+virtualenv -p python3 venv
+source venv/bin/activate
+
+pip install python-dotenv
+pip install bootstrap-flask
+pip install flask-sqlalchemy
+pip install flask-wtf 
+pip install flask-login
+pip install pyyaml
